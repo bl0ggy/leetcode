2176f1ec4c9e30085f67d5d4a4b812c0825aa420
Jessy Simeon <jessys34@hotmail.fr>
2024-11-11 07:59:46 +0900

    3097 solution on leetcode

--- src/3097.cpp
+++ src/3097.cpp
@@ -4,18 +4,42 @@ class Solution {
   public:
     int minimumSubarrayLength(vector<int> &nums, int k) {
         size_t numsSize = nums.size();
-        int minSize = INT_MAX;
-        int numOr;
-        for (int i = 0; i < numsSize; i++) {
-            numOr = 0;
-            for (int j = i; j < numsSize; j++) {
-                if ((numOr |= nums[j]) >= k) {
-                    minSize = min(minSize, j - i + 1);
+        size_t minSize = INT_MAX;
+        size_t start = 0, end = 0;
+        vector<int> bitCounts(32, 0);
+        while (end < numsSize) {
+            for (int i = 0; i < 32; i++) {
+                int num = nums[end];
+                if ((num >> i) & 1) {
+                    bitCounts[i]++;
                 }
             }
+
+            while (start <= end && bitCountsToNum(bitCounts) >= k) {
+                minSize = min(minSize, end - start + 1);
+                for (int i = 0; i < 32; i++) {
+                    int num = nums[start];
+                    if ((num >> i) & 1) {
+                        bitCounts[i]--;
+                    }
+                }
+                start++;
+            }
+
+            end++;
         }
 
         return minSize == INT_MAX ? -1 : minSize;
+    }
+
+    int bitCountsToNum(vector<int> &bitCounts) {
+        int ret = 0;
+        for (int i = 0; i < 32; i++) {
+            if (bitCounts[i]) {
+                ret |= 1 << i;
+            }
+        }
+        return ret;
     }
 };
 

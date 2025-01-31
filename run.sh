64057568a1a3f817be9bea02996218dbe157f91f
Jessy Simeon <jessys34@hotmail.fr>
2025-01-31 11:09:34 +0100

    Update run.sh to clear the terminal and add a dry run option

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+reset
+
 # Specify the directory containing the build.ninja file
 build_dir="build"
 # Specify the directory containing the binaries
@@ -7,6 +9,8 @@ tests_binary_dir="$build_dir/tests"
 
 # Flag to determine if compilation is needed
 compile_flag=false
+# Flag to determine if we must not run the tests
+dry_run=false
 
 # Function to run an executable
 run() {
@@ -31,19 +35,23 @@ run() {
 executables=()
 while [[ $# -gt 0 ]]; do
     case $1 in
-        -h|--help)
-            echo "Usage: $0 [-c] <executable_name1> [executable_name2] ..."
-            echo "  -c, --compile    Compile executables before running"
-            exit 1
-            ;;
-        -c|--compile)
-            compile_flag=true
-            shift
-            ;;
-        *)
-            executables+=("$1")
-            shift
-            ;;
+    -h | --help)
+        echo "Usage: $0 [-c] <executable_name1> [executable_name2] ..."
+        echo "  -c, --compile    Compile executables before running"
+        exit 1
+        ;;
+    -c | --compile)
+        compile_flag=true
+        shift
+        ;;
+    -d | --dry-run)
+        dry_run=true
+        shift
+        ;;
+    *)
+        executables+=("$1")
+        shift
+        ;;
     esac
 done
 
@@ -65,9 +73,13 @@ if $compile_flag; then
     fi
 fi
 
+if [ "$dry_run" = true ]; then
+    exit 0
+fi
+
 # If not executables proved, take all executable names from the build dir
 if [ ${#executables[@]} -eq 0 ]; then
-    pushd "$tests_binary_dir" > /dev/null
+    pushd "$tests_binary_dir" >/dev/null
     pwd
     for file in *; do
         # Check if the file is executable
@@ -75,13 +87,14 @@ if [ ${#executables[@]} -eq 0 ]; then
             executables+=("$file")
         fi
     done
-    popd > /dev/null
+    popd >/dev/null
 fi
 
 if [ ${#executables[@]} -eq 0 ]; then
     echo "No tests to run"
     exit 1
 fi
+
 # Then run the executables
 for executable_name in "${executables[@]}"; do
     run "$executable_name"

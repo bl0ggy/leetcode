a4ef0821d55a1d142ac9c4ad4e61be0e5ac5c988
Jessy Simeon <jessys34@hotmail.fr>
2025-02-20 09:44:56 +0900

    Solution 1980 Original test cases fail because multiple output values can pass

---
+++ src/1980.cpp
@@ -0,0 +1,83 @@
+#include "common.h"
+
+class Solution {
+  public:
+    string findDifferentBinaryString(vector<string> &nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+
+        if (find(nums.front().begin(), nums.front().end(), '1') != nums.front().end()) {
+            return string(n, '0');
+        }
+
+        if (find(nums.back().begin(), nums.back().end(), '0') != nums.back().end()) {
+            return string(n, '1');
+        }
+
+        int i = 0;
+        string a = nums[i];
+        do {
+            add(a);
+            i++;
+        } while (a == nums[i]);
+        return a;
+    }
+
+    bool add(string &s) {
+        int l = s.length();
+        int i = l - 1;
+
+        // Increment
+        s[i]++;
+        while (s[i] > '1') {
+            s[i] = '0';
+            if (i == 0) {
+                return false;
+            }
+            s[i - 1]++;
+            i--;
+        }
+
+        // Reached '11...1'
+        if (i == 0 && s[i] > '1') {
+            return false;
+        }
+        return true;
+    }
+};
+
+int main() {
+    TestSuite<string, vector<string>> testSuite("Binary string", "nums");
+    testSuite.addTestCases({
+        // Original test cases
+        {
+            "11",
+            {"01", "10"},
+        },
+        {
+            "11",
+            {"00", "01"},
+        },
+        {
+            "101",
+            {"111", "011", "001"},
+        },
+        // Submission test cases
+        // My test cases
+        {
+            "011",
+            {"000", "001", "010"},
+        },
+        {
+            "000",
+            {"101", "110", "111"},
+        },
+        {
+            "010",
+            {"000", "001", "111"},
+        },
+    });
+
+    Main main;
+    main.runTests(&Solution::findDifferentBinaryString, testSuite);
+}

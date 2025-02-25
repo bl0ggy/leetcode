410a8e4ef43157e8a168e408acb7697b3a2ec6c5
Jessy Simeon <jessys34@hotmail.fr>
2025-02-25 11:09:52 +0900

    Solution 1524

--- src/1524.cpp
+++ src/1524.cpp
@@ -2,45 +2,23 @@
 
 class Solution {
   public:
-    // Memory limit exceeded
-    // and probably Time limit exceeded too
     int numOfSubarrays(vector<int> &arr) {
         const int mod = 1000000007;
         int size = arr.size();
-        vector<vector<int>> dp(size + 1, vector<int>(size + 2, 0));
-        // 1 2 3
-        // 1     = odd
-        // 1 2   = odd
-        // 1 2 3 = even
-        // 2     = even
-        // 2 3   = odd
-        // 3     = odd
-        // [0 1 3 6 0]
-        // [0 0 2 5 0]
-        // [0 0 0 3 0]
-
-        int count = 0;
+        vector<int> dp(size + 1, 0);
+        int countEven = 0, countOdd = 0, countTotal = 0;
         for (int col = 0; col < size; col++) {
-            dp[0][col + 1] = dp[0][col] + arr[col];
-            if ((dp[0][col + 1] & 1)) {
-                count++;
-                if (count > mod) {
-                    count -= mod;
-                }
+            dp[col + 1] = dp[col] + arr[col];
+            if (dp[col + 1] & 1) {
+                countOdd++;
+                countTotal += 1 + countEven;
+            } else {
+                countEven++;
+                countTotal += countOdd;
             }
+            countTotal = countTotal % mod;
         }
-        for (int row = 1; row < size + 1; row++) {
-            for (int col = row; col < size + 1; col++) {
-                dp[row][col] = dp[row - 1][col] - dp[row - 1][row];
-                if ((dp[row][col] & 1)) {
-                    count++;
-                    if (count > mod) {
-                        count -= mod;
-                    }
-                }
-            }
-        }
-        return count;
+        return countTotal;
     }
 };
 

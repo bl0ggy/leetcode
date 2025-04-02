849bd782df471b2ffd98ea65a624cc5f688de42d
Jessy Simeon <jessys34@hotmail.fr>
2025-04-02 10:31:25 +0900

    2873 with 1 loop

--- src/2873.cpp
+++ src/2873.cpp
@@ -4,28 +4,14 @@ class Solution {
   public:
     long long maximumTripletValue(vector<int> &nums) {
         int size = nums.size();
-        long long min = LLONG_MAX, max = nums[0];
-        long long maxDiff = 0;
         long long ans = 0;
-        vector<long long> diffs(size), maxK(size);
-        maxK[size - 1] = nums[size - 1];
+        int maxNum = nums[0], maxDiff = 0;
 
         // Find max diff and max k
         for (int i = 1; i < size - 1; i++) {
-            maxK[size - 1 - i] = std::max(maxK[size - i], (long long)nums[size - 1 - i]);
-            if (nums[i] < min) {
-                min = nums[i];
-                maxDiff = max - min;
-            }
-            if (nums[i] > max) {
-                max = nums[i];
-                min = LLONG_MAX;
-            }
-            diffs[i] = maxDiff;
-        }
-
-        for (int i = 1; i < size - 1; i++) {
-            ans = std::max(ans, diffs[i] * maxK[i + 1]);
+            maxDiff = max(maxDiff, maxNum - nums[i]);
+            ans = max(ans, (long long)maxDiff * nums[i + 1]);
+            maxNum = max(maxNum, nums[i]);
         }
 
         return ans > 0 ? ans : 0;

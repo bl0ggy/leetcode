112fa0c5890465910e89058426e011593d4232d3
Jessy Simeon <jessys34@hotmail.fr>
2025-04-15 12:34:47 +0200

    Add modpow to algorithms.h

--- src/algorithms.h
+++ src/algorithms.h
@@ -19,3 +19,17 @@ inline vector<bool> sieveOfEratosthenes(size_t n) {
 
     return prime;
 }
+
+inline long long modpow(long long base, long long exp, long long modulus) {
+    long long res = 1;
+    base = base % modulus;
+    if (base == 0)
+        return 0;
+    while (exp > 0) {
+        if (exp & 1)
+            res = (res * base) % modulus;
+        exp = exp >> 1; // y = y/2
+        base = (base * base) % modulus;
+    }
+    return res;
+}
